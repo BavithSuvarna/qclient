0d033b5198699d6ba7d211e5d72daa067855ea4e
Bavith <suvarnabavith@gmail.com>
2025-06-19 22:22:21 +0530

    password changed

--- src/AdminLogin.js
+++ src/AdminLogin.js
@@ -10,7 +10,7 @@ function AdminLogin() {
   const handleLogin = (e) => {
     e.preventDefault();
 
-    if (username === 'admin' && password === '1234') {
+    if (username === 'Bavith' && password === '9257') {
       localStorage.setItem('adminLoggedIn', 'true'); // ✅ store flag
       navigate('/admin/panel');
     } else {
